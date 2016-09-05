dae852e3a7cde432a652d0102a4cf659c2c33957
Victor Lo <Victor@victors-macbook-air.local>
2016-09-04 22:53:48 -0700

    RGB sliders

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var purpleSliders: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var colorSquare: UIView!
+    @IBAction func updateBackGroundColor () {
+    
+    
+    let purple = CGFloat(purpleSliders.value) //i used purple just to test if the color changes in any way.
+    let blue = CGFloat(blueSlider.value)
+    let green = CGFloat(greenSlider.value)
+    
+      colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        
+     colorSquare.layer.borderWidth = 1
+        
+     colorSquare.backgroundColor = UIColor(red: purple, green: green, blue: blue, alpha: 1)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBackGroundColor()
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
